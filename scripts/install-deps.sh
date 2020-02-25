da5dba6a4d6e774bfc2a3edab63b2cb409451df1
Botho Hohbaum <bh@miu08.de>
2020-02-25 14:18:12 +0100

    optimized dependency installer script

--- scripts/install-deps.sh
+++ scripts/install-deps.sh
@@ -2,5 +2,5 @@
 
 # this should resolve all required dependencies automatically
 
-sudo apt install make build-essential gcc g++ llvm clang qt5-qmake qtchooser qtbase5-dev-tools libqt5websockets5-dev
+apt install make build-essential gcc g++ llvm clang qt5-default qt5-qmake qtchooser qtbase5-dev-tools libqt5websockets5-dev
 
