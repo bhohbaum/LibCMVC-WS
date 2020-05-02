369088d07ab2ed7b91343ce11f0d0a93b24a3fc6
Botho Hohbaum <bh@miu08.de>
2020-05-02 14:18:07 +0200

    build script deploys to vweb05 and libo. additionally it commits current qtws binaries into the joe nimble backend repo + a further commit with the same message is created for the current state of the qtws repo.

--- scripts/build.sh
+++ scripts/build.sh
@@ -15,14 +15,25 @@ rsync -avoze ssh --delete LibCMVC-WS root@vweb05.kmundp.de:/root/tmp/
 ssh root@vweb05.kmundp.de "/bin/bash -c 'cd /root/tmp/build ; make clean ; make qmake_all ; make -j 4 ; make install ; killall qtwsserver'"
 ssh root@vweb05.kmundp.de "/bin/bash -c 'rm -v /var/www/benimble/bin/qtws* ; cp -arv /usr/local/bin/qtws* /var/www/benimble/bin/ '"
 ssh root@vweb05.kmundp.de "/root/scripts/benimble/wsloop.sh & exit" &
+sleep 5
 kill -9 `ps -ef | grep "[s]sh root@vweb05.kmundp.de" | awk '{print $2}'`
 
 scp -r root@vweb05.kmundp.de:/var/www/benimble/bin/qtws* /home/botho/git/joe-nimble-app-backend-cms/bin/
-cd /home/botho/git/joe-nimble-app-backend-cms/
-git add ./bin/qtws*
-git commit -m "websocket server/client binaries updated."
-git pull
-git push
+
+if [ ! -z "$1" ]
+then
+	cd /home/botho/git/joe-nimble-app-backend-cms/
+	git add ./bin/qtws*
+	git commit -m "$@"
+	git pull
+	git push
+
+	cd /home/botho/git/LibCMVC-WS/
+	git add .
+	git commit -m "$@"
+	git pull
+	git push
+fi
 
 cd $PWD
 
