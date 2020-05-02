ddc77434c5da4f336f47044067393bfc6326c5eb
Botho Hohbaum <bh@miu08.de>
2020-05-02 04:00:59 +0200

    update

--- scripts/build.sh
+++ scripts/build.sh
@@ -9,7 +9,7 @@ make -j 4
 sudo make install
 cd ..
 scp -r LibCMVC-WS root@vweb05.kmundp.de:/root/tmp/
-ssh root@vweb05.kmundp.de bash -c 'cd /root/tmp/build ; make clean ; make qmake_all ; make ; make install ; killall qtwsserver ; /root/scripts/benimble/wsloop.sh &'
+ssh root@vweb05.kmundp.de "bash -c 'cd /root/tmp/build ; make clean ; make qmake_all ; make ; make install ; killall qtwsserver ; /root/scripts/benimble/wsloop.sh &'"
 
 cd $PWD
 
